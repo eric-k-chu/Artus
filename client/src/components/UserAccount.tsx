3efdea2dc47dda969365be3d4af9be264c6f7790
eric-k-chu <kiyuen88ec@gmail.com>
2023-11-16 05:17:05 +0000

    WIP

--- client/src/components/UserAccount.tsx
+++ client/src/components/UserAccount.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { IoPerson } from "react-icons/io5";
+import {
+  RiFolderVideoFill,
+  RiAccountBoxFill,
+  RiLogoutBoxFill,
+} from "react-icons/ri";
 
 export function UserAccount() {
   const [showSideBar, setShowSideBar] = useState(false);
@@ -12,11 +17,19 @@ export function UserAccount() {
         onClick={() => setShowSideBar(!showSideBar)}
       />
       {showSideBar && (
-        <div className="absolute right-0 top-8 flex h-96 w-48 flex-col items-center rounded-xl bg-white shadow-2xl dark:bg-void">
-          <h2>test</h2>
-          <h2>test</h2>
-          <h2>test</h2>
-          <h2>test</h2>
+        <div className="absolute right-8 top-0 flex h-96 w-48 flex-col items-center rounded-xl bg-white py-3 shadow-2xl dark:bg-void">
+          <div className="flex w-full basis-1/12 items-center gap-x-4 px-6 py-2 transition-none hover:cursor-pointer hover:bg-black/10">
+            <RiAccountBoxFill size={24} />
+            <span>Profile</span>
+          </div>
+          <div className="flex w-full basis-1/12 items-center gap-x-4 px-6 py-2 transition-none hover:cursor-pointer hover:bg-black/10">
+            <RiFolderVideoFill size={24} />
+            <span>Manage</span>
+          </div>
+          <div className="flex w-full basis-1/12 items-center gap-x-4 px-6 py-2 transition-none hover:cursor-pointer hover:bg-black/10">
+            <RiLogoutBoxFill size={24} />
+            <span>Sign Out</span>
+          </div>
         </div>
       )}
     </div>
